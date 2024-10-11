3e03df1cf8a718dd8d05ade4763ebbd0ecacc43b
rreal-de <lello.coelho@gmail.com>
2024-10-11 05:07:29 -0300

    padrao moulinette

--- Piscine_Reloaded/ex20/ft_strdup.c
+++ Piscine_Reloaded/ex20/ft_strdup.c
@@ -6,13 +6,9 @@
 /*   By: rreal-de <rreal-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 03:04:26 by rreal-de          #+#    #+#             */
-/*   Updated: 2024/10/11 05:05:18 by rreal-de         ###   ########.fr       */
+/*   Updated: 2024/10/11 05:07:02 by rreal-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
 char	*ft_strdup(char *src)
 {
@@ -21,7 +17,6 @@ char	*ft_strdup(char *src)
 
 	if (src == NULL)
 		return (NULL);
-
 	calc = malloc(strlen(src) + 1);
 	if (calc == NULL)
 	{
@@ -35,16 +30,3 @@ char	*ft_strdup(char *src)
 	*calc = '\0';
 	return (duplicate);
 }
-
-int	main(void)
-{
-	char	*str_src;
-	char	*str_cpy;
-
-	str_src = "teste de copia!!";
-	str_cpy = ft_strdup(str_src);
-	printf("Origem --> %s\n", str_src);
-	printf("Copia --> %s\n", str_cpy);
-}
-
-
