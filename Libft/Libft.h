28807a22946254550e7f9794668dfb6c644c67f0
rreal-de <lello.coelho@gmail.com>
2024-11-07 17:38:27 -0300

    arrumando a merda2

---
+++ Libft/Libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rreal-de <rreal-de@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 17:33:59 by rreal-de          #+#    #+#             */
+/*   Updated: 2024/11/07 17:37:29 by rreal-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stddef.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_isprint(int c);
+int		ft_isascii(int c);
+int		ft_isalnum(int c);
+int		ft_atoi(const char *str);
+int		ft_strncmp(const char *str_1, const char *str_2, size_t n);
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+#endif
