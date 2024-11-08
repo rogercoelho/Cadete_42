09bf82239c7058f25830b7b6e6079e2010e9882b
Lello Coelho - Note <lello.coelho@gmail.com>
2024-11-08 03:23:51 -0300

    parei no memchr e faltam 4

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: lello <lello@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 17:33:59 by rreal-de          #+#    #+#             */
-/*   Updated: 2024/11/08 03:04:20 by lello            ###   ########.fr       */
+/*   Updated: 2024/11/08 03:22:59 by lello            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@ char	*ft_strrchr(const char *string, int c);
 void	*ft_memcpy(void *dest, const void *src, size_t len);
 void	*ft_memmove(void *dest, const void *src, size_t len);
 int		ft_memcmp(const void *s1, const void *s2, size_t len);
+void	*ft_memchr(const void *str, int c, size_t len);
+
 
 
 #endif
