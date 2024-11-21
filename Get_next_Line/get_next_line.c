c27db2274e13ae3086d5c29807ff237597ed383b
rreal-de <lello.coelho@gmail.com>
2024-11-21 20:15:44 -0300

    GNL - Subdividindo funcoes para passar pela norminette - esta com looping infinito.

--- Get_next_Line/get_next_line.c
+++ Get_next_Line/get_next_line.c
@@ -3,132 +3,132 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lello <lello@student.42.fr>                +#+  +:+       +#+        */
+/*   By: rreal-de <rreal-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 21:09:42 by lello             #+#    #+#             */
-/*   Updated: 2024/11/21 14:17:00 by lello            ###   ########.fr       */
+/*   Updated: 2024/11/21 19:25:43 by rreal-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_create_line(size_t read_chars, char *str)
+char	*ft_create_line(int *read_chars, char *str, int *i)
 {
 	char	*str_return;
-	size_t	i;
+	int		j;
 
-	i = 0;
 	if (read_chars == 0)
 		return (NULL);
-	str_return = malloc(read_chars + 1);
+	str_return = (char *)malloc((*read_chars) + 1);
 	if (!str_return)
 		return (NULL);
-	while (i <= read_chars)
+	j = 0;
+	while (j < *read_chars && str[*i] != '\n')
 	{
-		if (str[i] == '\n')
-		{
-			str_return[i] = '\n';
-			str_return[i + 1] = '\0';
-			break ;
-		}
-		str_return[i] = str[i];
-		i++;
+		str_return[j] = str[*i];
+		j++;
+		(*i)++;
 	}
-	str_return[i] = '\n';
-	str_return[i + 1] = '\0';
+	if (str[*i] == '\n')
+	{
+		str_return[j] = '\n';
+		j++;
+	}
+	str_return[j] = '\0';
 	return (str_return);
 }
 
 void	ft_include_node(t_list *list, char *line)
 {
-	while (list->next_node != NULL)
-		list = list ->next_node;
-
-	list -> next_node = malloc(sizeof(t_list));
-	if (!list -> next_node)
-		return ;
-	list -> next_node -> content = line;
-	list -> next_node ->next_node = NULL;
+	list->content = line;
+	list->next_node = NULL;
 }
 
+static int	node_verify(t_list *list)
+{
+	if (list->content == NULL)
+		return (0);
+	return (1);
+}
 
-
-char	*get_next_line(int fd)
+char	*validate(int *i, int *read_chars, char *str, char *line)
 {
 	static t_list	*list;
-	char			*str;
-	char			*line;
-	size_t			read_chars;
-	size_t			i;
 
-	read_chars = 0;
-	i = 0;
-	str = malloc(BUFFER_SIZE + 1);
-	line = malloc(BUFFER_SIZE + 2);
 	list = malloc(sizeof(t_list));
-	if (!list)
-		return (NULL);
-	read_chars = read(fd, str, BUFFER_SIZE);
-	if (read_chars < 0)
-	{
-		free(str);
-		free(line);
-		free(list);
-		return (NULL);
-	}
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-
-	line = ft_create_line (read_chars, &str[i]);
-	ft_include_node(list, line);
+	node_verify(list);
 	while (i <= read_chars)
 	{
-		if (str[i] == '\n')
+		if (str[*i] != '\n')
 		{
-			line = ft_create_line (read_chars, &str[i + 1]);
+			line = ft_create_line (read_chars, str, i);
 			ft_include_node(list, line);
+			break ;
+		}
+		if (str[*i] == '\n')
+		{
+			i++;
+			if (str[*i] == '\0')
+				return (NULL);
+			line = ft_create_line (read_chars, str, i);
+			ft_include_node(list, line);
+			break ;
 		}
 		i++;
 	}
+	free(list);
+	return (line);
+}
 
+char	*get_next_line(int fd)
+{
+	static char	*str;
+	char		*line;
+	static int	read_chars;
+	static int	i;
+
+	line = malloc(BUFFER_SIZE + 2);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (read_chars == 0)
+	{
+		str = malloc(BUFFER_SIZE + 1);
+		read_chars = read(fd, str, BUFFER_SIZE);
+	}
+	if (read_chars < 0)
+		return (free(str), free(line), NULL);
+	line = validate(&i, &read_chars, str, line);
 	return (line);
 }
 
 
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <fcntl.h> // Para a função open
+#include <fcntl.h>  // Para open()
+#include <stdio.h>  // Para printf()
+#include <stdlib.h> // Para free()
+#include "get_next_line.h"
 
+int main(void)
+{
+    int fd;
+    char *line;
 
-// int main(void)
-// {
-//     int fd;
-//     char *result;
+    // Abrir o arquivo "test.txt" no modo somente leitura
+    fd = open("test.txt", O_RDONLY);
+    if (fd < 0)
+    {
+        perror("Erro ao abrir o arquivo");
+        return (1);
+    }
 
-//     // Abrindo um arquivo de texto em modo de leitura
-//     fd = open("./test.txt", O_RDONLY);
-//     if (fd == -1)
-//     {
-//         perror("Erro ao abrir o arquivo");
-//         return (1);
-//     }
+    // Ler e imprimir cada linha até que `get_next_line` retorne NULL
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s", line); // Imprimir a linha
+        free(line);         // Liberar a memória alocada por `get_next_line`
+    }
 
-//     // Chamando a função get_next_line
-//     result = get_next_line(fd);
+    // Fechar o arquivo
+    close(fd);
 
-//     // Verificando e imprimindo o resultado (se for aplicável)
-//     if (result)
-//     {
-//         printf("Linha lida: %s\n", result);
-//         free(result);
-//     }
-//     else
-//     {
-//         printf("Nenhuma linha lida ou erro ocorreu.\n");
-//     }
-
-//     // Fechando o arquivo
-//     close(fd);
-
-//     return (0);
-// }
+    return (0);
+}
